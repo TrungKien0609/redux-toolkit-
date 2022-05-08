db8d3722aa42cc1773a80db446a7948e541cdcb3
TrungKien <trungkien7300>
2022-05-09 01:21:14 +0700

    redux thunk

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import { Typography, Divider } from 'antd';
-import './App.css';
-import TodoList from './components/TodoList';
-import Filters from './components/Filters';
+import { Typography, Divider } from "antd";
+import "./App.css";
+import TodoList from "./components/TodoList";
+import Filters from "./components/Filters";
+import { setupServer } from "./fake-api";
+import { useEffect } from "react";
 
 const { Title } = Typography;
-
+setupServer();
 function App() {
+  useEffect(() => {
+    fetch("/api/todos", {
+      method: "POST",
+      body: JSON.stringify({
+        id: 4,
+        name: "lean react4",
+        completed: false,
+        priority: "Medium",
+      }),
+    })
+      .then((res) => fetch("/api/todos"))
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+    fetch("/api/todos")
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+
+    fetch("/api/updateTodo", {
+      method: "POST",
+      body: JSON.stringify({
+        id: 4,
+        name: "lean heheh",
+        completed: true,
+        priority: "Medium",
+      }),
+    }).then(() => {
+      fetch("/api/todos")
+        .then((res) => res.json())
+        .then((res) => console.log(res));
+    });
+  });
   return (
     <div
       style={{
         width: 500,
-        margin: '0 auto',
-        display: 'flex',
-        flexDirection: 'column',
-        backgroundColor: 'white',
+        margin: "0 auto",
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "white",
         padding: 20,
-        boxShadow: '0 0 10px 4px #bfbfbf',
+        boxShadow: "0 0 10px 4px #bfbfbf",
         borderRadius: 5,
-        height: '90vh',
+        height: "90vh",
       }}
     >
-      <Title style={{ textAlign: 'center' }}>TODO APP with REDUX</Title>
+      <Title style={{ textAlign: "center" }}>TODO APP with REDUX</Title>
       <Filters />
       <Divider />
       <TodoList />
@@ -29,3 +62,4 @@ function App() {
 }
 
 export default App;
+//34.56
